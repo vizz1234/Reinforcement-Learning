3a574763a2c9165fd80f58ef24f38777b8f4a6e7
vizz1234 <34003673+vizz1234@users.noreply.github.com>
2025-08-07 20:59:37 +0530

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -21,3 +21,37 @@ def simulate(runs, steps, bandits, epsilon):
   optimalActionCounts /= runs
 
   return rewardsAvg, optimalActionCounts, np.mean(np.array(trueBestReward))
+
+bandits = 10
+steps = 1000
+runs = 2000
+epsilons = [2]
+rewards, optimalCounts, labels, trueReward = [], [], [], []
+for eps in epsilons:
+    avgR, optA, tR = simulate(runs, steps, bandits, eps)
+    rewards.append(avgR)
+    optimalCounts.append(optA)
+    trueReward.append(tR)
+
+plt.figure(figsize=(12, 5))
+# for i in range(len(experiments)):
+plt.plot(rewards[0], label='UCB with c = 2')
+plt.plot(rewards[2], label = 'ε = 0.1 greedy')
+plt.axhline(y=np.mean(np.array(tR)), color='red', linestyle='--', linewidth=2, label='Avg True Best Reward')
+plt.xlabel("Steps")
+plt.ylabel("Average Reward")
+plt.title("Average Reward over Time")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plot: Optimal Action %
+plt.figure(figsize=(12, 5))
+plt.plot(optimalCounts[0], label = 'UCB with c = 2')
+plt.plot(optimalCounts[2], label= 'ε = 0.1 greedy')
+plt.xlabel("Steps")
+plt.ylabel("% Optimal Action Chosen")
+plt.title("Optimal Action Percentage over Time")
+plt.legend()
+plt.grid(True)
+plt.show()
